e46e6093a55966c6e1e2c061edc22b663e050f3a
guswlsl <111550606+guswlsl@users.noreply.github.com>
2025-08-05 16:44:16 +0900

    feat: 로그인, 회원가입 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-
+import Login from './pages/login/login.jsx'
+import Signup from './pages/login/signup.jsx'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' />
+                  <Route path='/' />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
             </Routes>
         </BrowserRouter>
     )
