387cd8d2ae4895845e1304c90adc2f94520d6953
wo0oz <128976011+wo0oz@users.noreply.github.com>
2025-08-23 01:37:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import VoiceUpload from './components/VoiceUpload';
-import Home from './components/Home'
-import Login from './pages/login/login.jsx'
-import Signup from './pages/login/signup.jsx'
-import VoiceRecord from './pages/detail/VoiceRecord.jsx'
-import VoiceInfo from './pages/detail/VoiceRecordInfo.jsx'
-import VoiceAlert from "./pages/detail/VoiceAlert.jsx"
-import Select from "./pages/select/select.jsx";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // 공용 페이지
 import Home from "./components/Home";
@@ -49,22 +41,5 @@ const App = () => {
     </BrowserRouter>
   );
 };
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/VoiceUpload' element={<VoiceUpload />} />
-                <Route path="/Home" element={<Home heroLogoSrc="/MainlogoWhite.png" />} /> {/* 만약 로고 바꿀거면 여기서 */}
-                <Route path='/Home' element={<Home />} />
-                <Route path='/' />
-                <Route path='/select' element={<Select />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<Signup />} />
-                <Route path='/voice-record' element={<VoiceRecord />} />
-                <Route path='/voice-info' element={<VoiceInfo />} />
-                <Route path='/voice-alert' element={<VoiceAlert />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
 
-export default App;
+export default App; 
