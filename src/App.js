4b266fcd29faf1e6829f4f83ddf91d2c06052538
JiYun Woo <loststar0910wju@naver.com>
2025-08-22 22:27:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import Signup from "./pages/login/signup";
 // 음성기록 (목록/상세)
 import VoiceRecordList from "./pages/detail/VoiceRecord";       // 목록
 import VoiceRecordDetail from "./pages/detail/TranscriptDetail"; // 상세
-
+import VoiceInfo from './pages/detail/VoiceRecordInfo.jsx'; 
+import VoiceAlert from './pages/detail/VoiceAlert.jsx'
 const App = () => {
   return (
     <BrowserRouter>
@@ -27,8 +28,10 @@ const App = () => {
 
         {/* 기능 */}
         <Route path="/voice-upload" element={<VoiceUpload />} />
-
+        <Route path="/voice-info" element={<VoiceInfo />} />
+        <Route path="/voice-alert" element={<VoiceAlert />} />
         {/* 음성 기록 */}
+        
         <Route path="/voice-record" element={<VoiceRecordList />} />
         <Route path="/voice-record/:id" element={<VoiceRecordDetail />} />
 
